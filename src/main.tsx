3a55a2e0cd14da9792f274c2a133f5a84d999ac0
彭于晏 <3405008677@qq.com>
2024-02-22 15:51:41 +0800

    updata file name

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
+import store from "@/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App title="App" />
+    <Provider store={store}>
+      <App title="App" />
+    </Provider>
   </React.StrictMode>
 );
